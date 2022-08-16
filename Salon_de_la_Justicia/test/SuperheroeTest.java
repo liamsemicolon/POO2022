4e70ac8d0d40ce35e63674cf29bd1c50cbf08c80
liamsemicolon <liamruizromero211@gmail.com>
2022-08-16 16:45:21 -0300

    sexy

---
+++ Salon_de_la_Justicia/test/SuperheroeTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SuperheroeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
+/* todo: un caso en el que el superheroe solo tenga asistentes, otro en el que solo tenga
+ * asistentes c/h, otro en donde tenga ambos
+ */
